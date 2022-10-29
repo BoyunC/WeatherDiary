c3d96df7a147c3c99571cc0680ae605f4c03ca86
BoyunC <cbyn0819@gmail.com>
2022-10-29 20:21:08 +0900

    3. 날씨 데이터 crud

--- src/main/java/com/ontheway/weatherdiary/repository/DiaryRepository.java
+++ src/main/java/com/ontheway/weatherdiary/repository/DiaryRepository.java
@@ -1,10 +1,18 @@
 package com.ontheway.weatherdiary.repository;
 
 import com.ontheway.weatherdiary.domain.Diary;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer>{
 
+	List<Diary> findAllByDate(LocalDate date);
+	List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+	Diary getFirstByDate(LocalDate date);
+	@Transactional
+	void deleteAllByDate(LocalDate date);
 }
