3ddeaa3d1641c07c5f15c841962bf1635805354f
BoyunC <cbyn0819@gmail.com>
2022-10-30 20:56:41 +0900

    6. 로그 남기기

--- src/main/java/com/ontheway/weatherdiary/service/DiaryService.java
+++ src/main/java/com/ontheway/weatherdiary/service/DiaryService.java
@@ -1,5 +1,6 @@
 package com.ontheway.weatherdiary.service;
 
+import com.ontheway.weatherdiary.WeatherDiaryApplication;
 import com.ontheway.weatherdiary.domain.DateWeather;
 import com.ontheway.weatherdiary.domain.Diary;
 import com.ontheway.weatherdiary.repository.DateWeatherRepository;
@@ -17,6 +18,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -32,6 +35,7 @@ public class DiaryService {
 
 	private final DiaryRepository diaryRepository;
 	private final DateWeatherRepository dateWeatherRepository;
+	private static final Logger logger = LoggerFactory.getLogger(WeatherDiaryApplication.class);
 
 	public DiaryService(DiaryRepository diaryRepository,
 		DateWeatherRepository dateWeatherRepository) {
@@ -42,6 +46,7 @@ public class DiaryService {
 	@Transactional
 	@Scheduled(cron = "0 0 1 * * *")
 	public void saveWeatherDate(){
+		logger.info("오늘도 날씨 데이터 잘 가져옴");
 		dateWeatherRepository.save(getWeatherFromApi());
 	}
 
@@ -63,6 +68,8 @@ public class DiaryService {
 
 	@Transactional(isolation = Isolation.SERIALIZABLE)
 	public void createDiary(LocalDate date, String text) {
+		logger.info("started to create diary");
+
 		// db에서 날씨 데이터 가져오기
 		DateWeather dateWeather = getDateWeather(date);
 
@@ -72,6 +79,7 @@ public class DiaryService {
 		nowDiary.setText(text);
 
 		diaryRepository.save(nowDiary);
+		logger.info("end to create diary");
 	}
 
 	private DateWeather getDateWeather(LocalDate date) {
@@ -86,6 +94,7 @@ public class DiaryService {
 
 	@Transactional(readOnly = true)
 	public List<Diary> readDiary(LocalDate date) {
+		logger.debug("read diary");
 		return diaryRepository.findAllByDate(date);
 	}
 
