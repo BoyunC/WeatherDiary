649523e91aeeb78d1dbe17da20c9e4797fad5639
BoyunC <cbyn0819@gmail.com>
2022-10-31 18:08:19 +0900

    8.Swagger 이용해 API document 만들기

--- src/main/java/com/ontheway/weatherdiary/controller/DiaryController.java
+++ src/main/java/com/ontheway/weatherdiary/controller/DiaryController.java
@@ -2,6 +2,8 @@ package com.ontheway.weatherdiary.controller;
 
 import com.ontheway.weatherdiary.domain.Diary;
 import com.ontheway.weatherdiary.service.DiaryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -22,21 +24,24 @@ public class DiaryController {
 		this.diaryService = diaryService;
 	}
 
-
+	@ApiOperation(value = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장", notes = "이것은 노트")
 	@PostMapping("/create/diary")
 	void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
 
 		diaryService.createDiary(date, text);
 	}
 
+	@ApiOperation(value = "선택한 날짜의 모든 일기 데이터를 가져옵니다")
 	@GetMapping("/read/diary")
 	List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate date) {
 
 		return diaryService.readDiary(date);
 	}
 
+	@ApiOperation(value = "선택한 기간의 모든 일기 데이터를 가져옵니다")
 	@GetMapping("/read/diaries")
-	List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate startDate, @RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate endDate){
+	List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = ISO.DATE) @ApiParam(value = "조회할 기간의 첫번째 날입니다.", example = "2022-05-05") LocalDate startDate,
+							@RequestParam @DateTimeFormat(iso = ISO.DATE) @ApiParam(value = "조회할 기간의 마지막 날입니다.", example = "2022-05-05") LocalDate endDate){
 
 		return diaryService.readDiaries(startDate, endDate);
 	}
